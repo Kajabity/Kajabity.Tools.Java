dce53bc72f94414ad58bb13bb366a6430e86b379
ivaylo5ev <ivaylo5ev@gmail.com>
2018-02-05 21:54:25 +0200

    * migrated to multi-targeting project using the dotnet sdk, removed deprecated project.json file * project now targets net20 and netstandard1.0, being the widest compatible range * updated .gitignore to exclude Visual Studio Code work directory

--- Kajabity.Tools.Java/Properties/AssemblyInfo.cs
+++ Kajabity.Tools.Java/Properties/AssemblyInfo.cs
@@ -30,7 +30,7 @@ using System.Resources;
 [assembly: AssemblyDescription( "Classes to read and write Java style “.properties” files in .NET applications." )]
 [assembly: AssemblyConfiguration( "" )]
 [assembly: AssemblyCompany( "Williams Technologies Limited" )]
-[assembly: AssemblyCopyright( "Copyright © 2009-17 Williams Technologies Limited." )]
+[assembly: AssemblyCopyright( "Copyright © 2009-18 Williams Technologies Limited." )]
 [assembly: AssemblyTrademark( "" )]
 [assembly: AssemblyCulture( "" )]
 
@@ -46,6 +46,6 @@ using System.Resources;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion( "0.1.*" )]
+[assembly: AssemblyVersion( "0.1.0.0" )]
 [assembly: AssemblyFileVersion( "0.1.0.0" )]
 
