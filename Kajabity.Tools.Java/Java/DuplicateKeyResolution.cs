6189d4963c23a699468b088eefad5b27ddfa5659
Roman Ilyenko <ilienko.roman@gmail.com>
2018-10-02 11:29:44 +0300

    Add configuration switch for the duplicate keys.

---
+++ Kajabity.Tools.Java/Java/DuplicateKeyResolution.cs
@@ -0,0 +1,40 @@
+﻿/*
+ * Copyright 2009-17 Williams Technologies Limited.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * Kajbity is a trademark of Williams Technologies Limited.
+ *
+ * http://www.kajabity.com
+ */
+
+namespace Kajabity.Tools.Java
+{
+    /// <summary>
+    /// Defines the strategy how to handle duplicate keys when reading the property file.
+    /// </summary>
+    public enum DuplicateKeyResolution
+    {
+        /// <summary>
+        /// Overwrite the key with a new value.
+        /// 
+        /// Last one wins.
+        /// </summary>
+        Overwrite = 1,
+
+        /// <summary>
+        /// Throw an excecwption.
+        /// </summary>
+        Throw = 2
+    }
+}
